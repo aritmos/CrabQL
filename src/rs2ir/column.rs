8b576aaf4bac824bc8fd3aa08ed3ca7e787e0b5f
Sebastian <52433836+aritmos@users.noreply.github.com>
2024-02-09 01:33:19 +0100

    🏷️🚧 Implement (`todo!`) `Comparison` for `Column`

--- src/rs2ir/column.rs
+++ src/rs2ir/column.rs
@@ -1,3 +1,9 @@
+use super::{
+    expr,
+    funcs::Functions,
+    value::{Comparison, Value},
+};
+
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Column<'c> {
     pub(super) name: &'c str,
@@ -6,5 +12,33 @@ pub struct Column<'c> {
 impl<'c> Column<'c> {
     pub(super) fn new(name: &'c str) -> Self {
         Self { name }
+    }
+}
+
+impl<'c> Comparison for Column<'c> {
+    type Expr = expr::Expr;
+
+    fn eq(&self, _val: impl Into<Value>) -> Self::Expr {
+        todo!()
+    }
+
+    fn ne(&self, _val: impl Into<Value>) -> Self::Expr {
+        todo!()
+    }
+
+    fn gt(&self, _val: impl Into<Value>) -> Self::Expr {
+        todo!()
+    }
+
+    fn ge(&self, _val: impl Into<Value>) -> Self::Expr {
+        todo!()
+    }
+
+    fn lt(&self, _val: impl Into<Value>) -> Self::Expr {
+        todo!()
+    }
+
+    fn le(&self, _val: impl Into<Value>) -> Self::Expr {
+        todo!()
     }
 }
