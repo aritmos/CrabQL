ddc49098b334e64e45b8d0f72cc6d87e0563340d
aritmos <52433836+aritmos@users.noreply.github.com>
2024-03-20 19:08:50 +0100

    🚧🏷️ Add case expressions
    WIP: `CoreExpr::conditions` is not fully implemented.
    
    While working on the functionality of the case expression, a better implementation of conditional checking came to mind.
    Iterators will be replaced by a concrete type that may contain one or more errors, as well as stack information or any other required data.
    This is a much better interface than the current implementation.

--- src/checker/cond.rs
+++ src/checker/cond.rs
@@ -1,7 +1,9 @@
 use crate::expr::ExprType;
 
 /// The conditions that the standard [`crate::checker::Checker`]s verify
+#[derive(Debug)]
 pub enum Condition {
     ColExists(String),
     ColExistsAndType(String, ExprType),
+    CaseRetErr((ExprType, ExprType)),
 }
